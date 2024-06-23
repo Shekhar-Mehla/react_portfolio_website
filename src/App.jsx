61fe61a540d493f7673340b3a79fa3e72aaa1e3c
Shekhar Singh <smehla147@gmail.com>
2024-06-23 21:08:14 +1000

    added the banner section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Header } from "./components/Header";
 import "./App.css";
 import { Hero } from "./components/Hero";
+import { Banner } from "./components/Banner";
 
 export const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Banner />
     </>
   );
 };
